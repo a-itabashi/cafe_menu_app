f482e09a4283088da5e9629a6aa2608d5048d5a7
Aguru itabashi <itabashi.a@gmail.com>
2019-06-14 18:43:05 +0900

    feat: ビジネスロジックを修正

--- app/controllers/api/v1/starbucks_controller.rb
+++ app/controllers/api/v1/starbucks_controller.rb
@@ -3,15 +3,19 @@ require 'nokogiri'
 
 class Api::V1::StarbucksController < ApplicationController
   def index
-    @names = []
-    @images = []
-    @prices = []
-    @links = []
+    @lists = []
+    url = "https://www.starbucks.co.jp/"
+    
     Starbucks::SearchService.new.run('https://www.starbucks.co.jp/new/lineup/?nid=mm&mode=cafe_pc_002', '.recommend') do |node|
-      @names << node.css('.productName').inner_html
-      @images << node.css('img').attribute('data-original').value
-      @prices << node.css('.productPrice').inner_html
-      @links << node.css('a').attribute('href').value
+      keys = ["name", "image", "price", "link"]
+
+      values = []
+      values << node.css('.productName').inner_html
+      values << (url + node.css('img').attribute('data-original').value)
+      values << node.css('.productPrice').inner_html
+      values << (url + node.css('a').attribute('href').value)
+      
+      @lists << Hash[keys.collect.zip(values)]
     end
   end
 end
