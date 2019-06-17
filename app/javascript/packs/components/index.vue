3afce86607e3e50a7a16d3b5efe19be2c582ddbc
Aguru itabashi <itabashi.a@gmail.com>
2019-06-17 18:18:59 +0900

    assetsにpathを通す

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -31,7 +31,7 @@
       <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d12966.84005395225!2d139.69729794229832!3d35.65951562561044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z5riL6LC344CA44K_44Oq44O844K644Kz44O844OS44O8!5e0!3m2!1sja!2sjp!4v1560747334579!5m2!1sja!2sjp" width=100% frameborder="0" style="border:0" allowfullscreen></iframe>
       <hr>
     </div>
-    <img class="img" src="/assets/cafe_img.jpg" />
+    <img class="img" src="/assets/cafe_img.jpg">
   </div>
 </template>
 
